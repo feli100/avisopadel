82f5581503046fd27406b4a8b7cc5a7a0d905686
feli100 <33816023+feli100@users.noreply.github.com>
2024-04-02 12:37:05 +0200

    Update bot_script.py

--- bot_script.py
+++ bot_script.py
@@ -1,7 +1,4 @@
 import requests
-import schedule
-import time
-from datetime import datetime
 import os
 
 def enviar_mensaje_telegram(mensaje):
@@ -12,15 +9,6 @@ def enviar_mensaje_telegram(mensaje):
     response = requests.post(url, data=datos)
     print(response.text)  # Para depuración
 
-def comprobar_y_enviar():
-    now = datetime.now()
-    # Asegúrate de ajustar esta condición para tu zona horaria específica, si es necesario
-    if now.weekday() == 1 and now.hour == 12 and now.minute == 1:  # 1 = Martes
-        enviar_mensaje_telegram("Hola, esto es una alarma de tu bot de Telegram.")
-
-# Programa la función para que se ejecute cada minuto
-schedule.every().minute.at(":00").do(comprobar_y_enviar)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# Mensaje que deseas enviar
+mensaje = "Hola, esto es una alarma para que alquiles la pista del próximo martes."
+enviar_mensaje_telegram(mensaje)
