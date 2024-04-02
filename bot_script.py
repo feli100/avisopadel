f69ab1803d8e697d3264acd2245f44bafb87e190
feli100 <33816023+feli100@users.noreply.github.com>
2024-04-02 15:20:15 +0200

    Update bot_script.py

--- bot_script.py
+++ bot_script.py
@@ -1,7 +1,7 @@
-import requests
-import os
+#import requests
+#import os
 
-def enviar_mensaje_telegram(mensaje):
+#def enviar_mensaje_telegram(mensaje):
     token = os.getenv("TELEGRAM_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
     url = f"https://api.telegram.org/bot{token}/sendMessage"
